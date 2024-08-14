5ab77d0f0e4f4aac1be3af0e476f2a4586e4c13b
EduardoFrs <fernandeseduardo00@gmail.com>
2024-08-14 08:45:45 +0200

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 """0. Basic annotations - add"""
 
-def add(a : float, b : float) -> float:
+
+def add(a: float, b: float) -> float:
+
     """add two float"""
     return a + b
