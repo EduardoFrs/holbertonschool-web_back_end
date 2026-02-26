ad74da45ef40296d0e4545e58318169e7f8eeb32
EduardoFrs <fernandeseduardo00@gmail.com>
2026-02-26 09:39:56 +0100

    feat: request validation

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,13 +2,15 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from api.v1.auth import auth
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -29,17 +31,36 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def not_found(error) -> str:
+def unauthorized(error) -> str:
     """ Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def not_found(error) -> str:
-    """ Unauthorized
+def forbidden(error) -> str:
+    """ "Forbidden"
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """Handler before_request"""
+    if auth is None:
+        return
+    if not auth.require_auth(
+        request.path, [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/"
+            ]
+    ):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
