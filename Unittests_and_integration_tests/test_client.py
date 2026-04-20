68fddc38bf0712beaec88aac72ff5d78653b4423
EduardoFrs <fernandeseduardo00@gmail.com>
2026-04-20 09:48:40 +0200

    feat: test public repo

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -24,3 +24,13 @@ class TestGithubOrgClient(unittest.TestCase):
         result = test_class.org
         self.assertEqual(result, {"login": input})
         mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
+    def test_public_repos_url(self):
+        """ Test the public repo url """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
