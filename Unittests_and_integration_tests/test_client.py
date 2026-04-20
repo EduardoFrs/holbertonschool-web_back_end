11033c52ef35b7f2445c37c084c77505b525feb3
EduardoFrs <fernandeseduardo00@gmail.com>
2026-04-20 09:42:43 +0200

    fix: add missing imports

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,17 +3,23 @@
 
 import unittest
 
+from unittest.mock import patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
 class TestGithubOrgClient(unittest.TestCase):
     """Gitghub ORG Client """
 
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ("google"),
+        ("abc")
     ])
     @patch('client.get_json')
-    def test_org(self, input, mock):
+    def test_org(self, input, mock_get_json):
         """ Test organization
         """
+        mock_get_json.return_value = {"login": input}
         test_class = GithubOrgClient(input)
         test_class.org()
-        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{input}')
