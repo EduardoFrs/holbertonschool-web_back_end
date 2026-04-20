d62c299566dda8f8c20cc822cbcfc14af0db1728
EduardoFrs <fernandeseduardo00@gmail.com>
2026-04-20 09:12:43 +0200

    feat: add assertraise

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test methode"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a"), None),
+        ({"a": 1}, ("a", "b"), None)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test methode"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path), expected
