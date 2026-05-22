19664d88ca03b304bcb087cf0565f41f3532604a
EduardoFrs <fernandeseduardo00@gmail.com>
2026-05-22 09:14:47 +0200

    feat: 0. Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,27 @@
+"""Redis"""
+import redis
+from typing import Callable, Optional, Union
+import uuid
+
+
+class Cache:
+    """Cache"""
+    def __init__(self):
+        """Init method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """Get value"""
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
