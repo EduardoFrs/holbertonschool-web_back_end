7034519d1aeb087eb1cf4eadf522551bea7c2c20
EduardoFrs <fernandeseduardo00@gmail.com>
2026-06-12 09:23:50 +0200

    feat: 3. Old school band

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -6,7 +6,7 @@ SELECT
     band_name,
     CASE
         WHEN split IS NOT NULL THEN split - formed
-        ELSE YEAR(CURRENT_DATE) - formed
+        ELSE 2024 - formed
     END as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
