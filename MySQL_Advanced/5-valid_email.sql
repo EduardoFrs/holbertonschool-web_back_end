caab15f4e78aa2eb131bfeb26f83050af451c62e
EduardoFrs <fernandeseduardo00@gmail.com>
2026-06-12 09:27:32 +0200

    feat:5. Email validation to sent

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- orm > sql
+-- commentaire
+
+
+CREATE TRIGGER reset_valid_email_before_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email <> NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
